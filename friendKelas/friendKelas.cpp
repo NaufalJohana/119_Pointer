051a5158b06234ed1f54dedba102740e47ed6b0c
NaufalJohana <naufaljohana@gmail.com>
2023-05-08 11:58:23 +0700

    membuat friend kelas

--- friendKelas/friendKelas.cpp
+++ friendKelas/friendKelas.cpp
@@ -1,20 +1,39 @@
-// friendKelas.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class siswa;
+
+class orang {
+private:
+	string nama;
+public:
+	void setNama(string pNama);
+	friend class siswa;
+};
+
+class siswa {
+private:
+	int id;
+public:
+	void setId(int pId);
+	void displayAll(orang& a);
+};
+
+void siswa::displayAll(orang& a) {
+	cout << id << endl << a.nama;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+void siswa::setId(int pId) {
+	id = pId;
+}
+int main() {
+	orang joko;
+	joko.setNama("Joko Kumat");
+	siswa joko_siswa;
+	joko_siswa.setId(1);
+	joko_siswa.displayAll(joko);
+	return 0;
+}
